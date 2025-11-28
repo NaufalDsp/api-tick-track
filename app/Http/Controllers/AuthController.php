fc325e72609897c2d6e51e7e5f44dd32512118d4
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-11-28 19:13:13 +0700

    feat: implement authentication with Sanctum and create login route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            if (!Auth::guard('web')->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Unauthorized',
+                    'data' => null
+                ], 401);
+            }
+
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login Berhasil',
+                'data' => [
+                    'token' => $token,
+                    'user' => $user
+                ]
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'error' =>$e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function me()
+    {
+        try {
+            $user = Auth::user();
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
